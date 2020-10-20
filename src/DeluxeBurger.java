4ff0e52c8b6835d3b8e7a597b2920afb0b4c5747
Darwin Tirta K <darwin.hermawan@gmail.com>
2020-10-20 15:25:42 +0700

    Java Online MasterClass Burger Challenge

--- src/DeluxeBurger.java
+++ src/DeluxeBurger.java
@@ -4,5 +4,19 @@ public class DeluxeBurger extends Hamburger{
 
     public DeluxeBurger(String breadRollType, int meat, double burgerPrice, String name) {
         super(breadRollType, meat, burgerPrice, name);
+        setAddition();
+    }
+
+    public void setAddition() {
+        chips = new Addition("chips", 8);
+        drinks = new Addition("drinks", 10);
+    }
+
+    public Addition getChips() {
+        return chips;
+    }
+
+    public Addition getDrinks() {
+        return drinks;
     }
 }
